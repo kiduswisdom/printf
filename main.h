6afae5024b3ef5893ab23d053ec5c8effc91e4b2
kiduswisdom <kidusjob@gmail.com>
2022-08-12 13:29:19 -0700

    printf

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * struct print - A structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+ 
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_b(va_list b);
+int print_u(va_list u);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+#endif
